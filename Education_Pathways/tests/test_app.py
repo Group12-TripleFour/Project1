98c2558c81296d4bd40cda47b8e6dfec1486b461
EmmaZhao0413 <emmazhao@LAPTOP-HDJSDEGP.localdomain>
2022-11-13 12:14:58 -0500

    add tests for filter result and course backend implementation

--- Education_Pathways/tests/test_app.py
+++ Education_Pathways/tests/test_app.py
@@ -1,68 +1,19 @@
-from app import app
+from index import app
+import controller
 from minor import check_course_in_minor
 from flask.testing import FlaskClient
 
 
-# Jean
-def test_check_course_in_minor():
-    course = "MIE439H1S"
+# Emma - test for filter functionality
+def test_filter_results():
     minor = "Biomedical Engineering Minor"
-    result = check_course_in_minor(course)
-
-    assert result == minor
-
-# Cansin
-def test_user_register_endpoint():
-    tester = app.test_client()
-    response = tester.get("/user/register")
-
-    assert response.status_code == 200
-
-def test_user_login_endpoint():
-    tester = app.test_client()
-    response = tester.get("/user/login")
-
-    assert response.status_code == 200
-
-def test_search_endpoint():
-    tester = app.test_client()
-    response = tester.get("/search")
-
-    assert response.status_code == 200
+    table =  controller.filter_results(None, "0", "Any", "Any", "Any", minor)
+    assert table[0].iloc[:,0][0].strip().split('<')[1].split('>')[1]=="BME440H1"
 
 def test_course_details_endpoint():
     tester = app.test_client()
-    response = tester.get("/course/details?code=ECE318H1")
+    response = tester.get("/course/BME440H1")
 
     assert response.status_code == 200
 
-def test_course_graph_endpoint():
-    tester = app.test_client()
-    response = tester.get("/course/graph?code=ECE318H1")
-
-    assert response.status_code == 200
-
-def test_user_wishlist_endpoint():
-    tester = app.test_client()
-    response = tester.get("/user/wishlist")
-
-    assert response.status_code == 200
-
-def test_user_wishlist_addCourse_endpoint():
-    tester = app.test_client()
-    response = tester.get("/user/wishlist/addCourse")
-
-    assert response.status_code == 200
-
-def test_user_wishlist_removeCourse_endpoint():
-    tester = app.test_client()
-    response = tester.get("/user/wishlist/removeCourse")
-
-    assert response.status_code == 200
-
-def test_user_wishlist_minorCheck_endpoint():
-    tester = app.test_client()
-    response = tester.get("/user/wishlist/minorCheck")
-
-    assert response.status_code == 200
 
