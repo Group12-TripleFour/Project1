40031c1bb925a53f6f78513f0a4a6eb55416aa2f
Nathan Coelho <nathan.coelho@mail.utoronto.ca>
2022-11-15 20:48:36 -0500

    Changing Navbar file back to original

--- Education_Pathways/frontend/src/components/Navbar.js
+++ Education_Pathways/frontend/src/components/Navbar.js
@@ -4,70 +4,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import logo from './img/logo.png'
 import { Navbar, Nav } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch, Link, useLocation } from "react-router-dom";
-// import LogIn from "./LogIn.jsx";
 import CourseDescriptionPage from "./CourseDescription";
-// import Wishlist from './Wishlist';
-// import SignUp from './SignUp'
-import SearchResultDisplay from './ResultDisplay';
+
+
+import SearchResultDisplay, { global_array } from './ResultDisplay'
 import Form from "./Form";
 import FeedbackList from './feedbacks/FeedbackList';
-
-import Comparison from './Comparison'
 import { useContext } from 'react';
-import FavoritesContext from './favorites-context'
-import classes from './Navbar.module.css'
+import FavoritesContext from './favorites-context';
+import classes from './Navbar.module.css';
 import FeedbackSubmitPage from './FeedbackSubmitPage';
 
-// import { initializeApp } from "firebase/app";
-// import { getDatabase, ref, child, get  } from "firebase/database";
-
-// // Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA6YzFrRBpdaOCf8xX3lZHPpYRDtYMH_7g",
-//   authDomain: "educationpathways-c37ec.firebaseapp.com",
-//   databaseURL: "https://educationpathways-c37ec-default-rtdb.firebaseio.com",
-//   projectId: "educationpathways-c37ec",
-//   storageBucket: "educationpathways-c37ec.appspot.com",
-//   messagingSenderId: "868723117829",
-//   appId: "1:868723117829:web:0ffafe21bca572ee2b716c"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// // Initialize Realtime Database and get a reference to the service
-// const database = getDatabase(app);
-
-// const dbRef = ref(getDatabase());
-// get(child(dbRef, `feedback/${course_name}`)).then((snapshot) => {
-//   if (snapshot.exists()) {
-//     console.log(snapshot.val());
-//   } else {
-//     console.log("No data available");
-//   }
-// }).catch((error) => {
-//   console.error(error);
-// });
-
-// const DUMMY_DATA = [
-//   {
-//     coursename: 'ABC',
-//     workload: 'H',
-//     complexity: 'H',
-//     usefulness: 'H',
-//   },
-//   {
-//     coursename: 'BCA200',
-//     workload: 'L',
-//     complexity:'L',
-//     usefulness: 'L',
-//   },
-//   {
-//     coursename: 'QWE200',
-//     workload: 'L',
-//     complexity:'L',
-//     usefulness: 'L',
-//   },
-// ];
+import NewFeedbackForm from './feedbacks/NewFeedbackForm';
 
 function CourseDescription (props) {
   let query = useQuery();
@@ -85,34 +33,14 @@ function Count(){
   console.log(favoritesCtx.totalFavorites);
   return (
     <span className={classes.badge}>{favoritesCtx.totalFavorites}</span> 
-  );
-}
+  )}
 
+function addFeedbackHandler(feedback){
+  console.log(feedback);
+}
 
 export default class NavbarComp extends Component {
   
-
-  constructor(props){
-    super(props)
-    this.state = {
-      course_code : this.props.course_code,
-      course_name: this.props.course_name,
-      username: localStorage.getItem('username'),
-      login: false
-    }
-  }
-
-  componentDidMount() {
-    if (localStorage.getItem('username') !== "") {
-      this.setState({username: localStorage.getItem('username')})
-    }
-  }
-
-  logOut = () => {
-    localStorage.setItem('username', "");
-    this.setState({username: ""})
-  }
-
   render() {
     return (
       <Router>
@@ -128,23 +56,22 @@ export default class NavbarComp extends Component {
             <Navbar.Toggle />
             <Navbar.Collapse>
               <Nav>
-                <Nav.Link as={Link} to="/about">
+			
+                <Nav.Link className={classes.navpath} as={Link} to="/about">
                   About Us
                 </Nav.Link>
 
-                { <Nav.Link as={Link} to="/filter">
-                  Filter
-                </Nav.Link> }
+		<a class="navbar-brand-top" href="/filter"> 
+		  Filter 
+		</a>
 
                 <Nav.Link as={Link} to="/review">
                   Review
                 </Nav.Link>
 
-                <Nav.Link as={Link} to="/comparison">
-                  Comparison
-                  <span className='badge'><Count/></span>
-                </Nav.Link>
-                
+    		<a class="navbar-brand-top" href="/Comparison"> 
+		  Comparison 
+		</a>
 
               </Nav>
             </Navbar.Collapse>
@@ -156,67 +83,38 @@ export default class NavbarComp extends Component {
             <div className = "body_text">
             <p>
 
-      Welcome to CARTE's in-development tool for course selection at UofT. Education Pathways allows for more intelligent course searching, by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
+      Welcome to Triple 4's in-development tool for course selection at UofT. Education Pathways allows for more intelligent course searching, by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
 
 Whatever year you are looking for, Education Pathways will also suggest courses in earlier years that will best help you to prepare. To get the most out of this, try searching for courses in a later year and see what is suggested for your current one.
 
-We are looking for feedback to improve Education Pathways and make it more useful for students. If you have ideas or suggestions, please <a href = "mailto:alex.olson@utoronto.ca">  email us! </a> <br></br>
+We are looking for feedback to improve Education Pathways and make it more useful for students. If you have ideas or suggestions, please <a href = "mailto:eric.liang@utoronto.ca">  email us! </a> <br></br>
 </p>
 <p> 
   <b>Development Team: </b>
 </p>
-<p>Alexander Olson <a href="https://carte.utoronto.ca/"> (CARTE)</a> </p>
-<p>Student team from <a href="https://shuiblue.github.io/UofT-ECE444/">ECE444-Fall2021</a> : Janelle Cuevas, Jean Lin, Terry Luan, Cansin Varol, Nick Woo</p>
+<p>Student team from <a href="https://github.com/Group12-TripleFour/Project1">ECE444-Fall2022</a> : Emma Zhao, Eric Liang, Mandy Hsu, Nathan Coelho</p>
 
 
       </div>
               {/* <SearchResultDisplay /> */}
             </Route>
 
-            {/* <Route path="/filter">
-              <div style={{ marginTop: "10%" }}>
-              <h2> Filter for courses </h2>
-              <Form />
-              </div>
-            </Route> */}
-
             <Route path="/review">
             <div>
               <FeedbackSubmitPage/>
-              {/* <section>
-                <FeedbackList feedbacks={DUMMY_DATA} />
-              </section> */}
             </div>
-            </Route>
-
-            <Route path="/comparison">
-              <Comparison/>
-            </Route>
-
-            <Route path="/test">
-             
-            </Route>
+            </Route>       
             
             <Route exact
               path="/courseDetails/:code"
-              render={props =>(<CourseDescriptionPage {...props} />)}
-              >
+              render={props =>(<CourseDescriptionPage {...props} />)}>
             </Route>
             <Route path="/">
               <SearchResultDisplay />
-              {/* <section>
-                <FeedbackList feedbacks={DUMMY_DATA}/>
-              </section> */}
             </Route>
-
-            
-            
 
           </Switch>
         </div>
-        
-        
-    
       </Router>
     );
   }
